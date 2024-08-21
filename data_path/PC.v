666a3062ed2a87188ff3d5e27f18f255e90ced99
Axel Rueda Gimenez <axelrue@localhost.localdomain>
2024-08-21 16:10:29 -0300

    add pc

---
+++ data_path/PC.v
@@ -0,0 +1,21 @@
+module PC (
+    input wire clk,
+    input wire [15:0] pcNext,
+    output wire [15:0] pc
+);
+
+    reg [15:0] s_pc;
+
+    // inicializacion en cero
+    initial begin
+        s_pc = 0;
+    end
+
+    // activacion por cambio de flanco
+    always @(posedge clk) begin
+        s_pc <= pcNext;
+    end
+
+    assign pc = s_pc;
+
+endmodule
