54258c28eaf4632ebf42a3da337478ad0e67f9c3
axelrueg <axelcuer@gmail.com>
2024-09-11 12:08:09 -0300

    refactor

--- data_path/ALU.v
+++ data_path/ALU.v
@@ -2,56 +2,22 @@ module ALU(
     input [31:0] srcA,
     input [31:0] srcB,
     input [2:0] ALUControl,
-    output [31:0] res,
+    output reg [31:0] res,
     output wire zero
 );
 
-reg [31:0] aux = 0;
-reg aux_zero = 0;
+assign zero = (res == 32'b0) ? 1'b1: 1'b0;
 
-always @(*)
-begin
+always @(*) begin
     case (ALUControl)
-        3'b000:
-            begin
-                aux = srcA+srcB;
-                aux_zero <= 0;
-            end
-        3'b001: 
-            begin
-                aux = srcA-srcB;
-                aux_zero <= 0;
-            end
-        3'b010:
-            begin
-                aux = srcA & srcB;
-                aux_zero <= 0;
-            end
-        3'b011:
-            begin
-                aux = srcA | srcB;
-                aux_zero <= 0;
-            end
-        // este no esta incluuido en la guia, puede ser util
-        3'b100:
-            begin
-                aux = (srcA == srcB);
-                aux_zero <= srcA == srcB;
-            end
-        3'b101: // SLT
-            begin
-                aux = srcB > srcA;
-                aux_zero <=  srcB > srcA;
-            end
-        default:
-            begin
-                aux = srcA;
-                aux_zero <= 0;
-            end
+        3'b000: res = srcA+srcB;
+        3'b001: res = srcA-srcB;
+        3'b010: res = srcA & srcB;
+        3'b011: res = srcA | srcB;
+        3'b100: res = (srcA == srcB);
+        3'b101: res = $signed(srcB) > $signed(srcA);
+        default: res = srcA;
     endcase
 end
-
-assign zero = aux_zero;
-assign res = aux;
 
 endmodule
