71a3f3249fb2ff69958fa7275dd26562447d9bad
axelrueg <axelcuer@gmail.com>
2024-09-10 21:05:42 -0300

    add outputs for csr in main decoder

--- control_unit/mainDeco.v
+++ control_unit/mainDeco.v
@@ -1,14 +1,15 @@
 /**
 ## RESUME
 
-| branch | jump | resSrc | memWrite | aluSrc | inmSrc | regWrite |aluOp  |
-|--------|------|--------|----------|--------|--------|----------|-------|
-| 0      | 0    | 2'b01  | 0        | 1      | 2'b00  | 1        | 2'b00 |  LW
-| 0      | 0    |        | 1        | 1      | 2'b01  | 0        | 2'b00 |  SW
-| 0      | 0    | 2'b00  | 0        | 0      |        | 1        | 2'b10 |  R-type
-| 1      | 0    |        | 0        | 0      | 2'b10  | 0        | 2'b01 |  B-type
-| 0      | 0    | 2'b00  | 0        | 1      | 2'b00  | 1        | 2'b10 |  I-Type
-| 0      | 1    | 2'b10  | 0        |        | 2'b11  | 1        |       |  jal
+| branch | jump | resSrc | memWrite | aluSrc | inmSrc | regWrite |aluOp  | mocsr |
+|--------|------|--------|----------|--------|--------|----------|-------|-------|
+| 0      | 0    | 2'b01  | 0        | 1      | 2'b00  | 1        | 2'b00 | 2'b00 |  LW     (3)
+| 0      | 0    |        | 1        | 1      | 2'b01  | 0        | 2'b00 | 2'b00 |  SW     (35)
+| 0      | 0    | 2'b00  | 0        | 0      |        | 1        | 2'b10 | 2'b00 |  R-type (51)
+| 1      | 0    |        | 0        | 0      | 2'b10  | 0        | 2'b01 | 2'b00 |  B-type (99)
+| 0      | 0    | 2'b00  | 0        | 1      | 2'b00  | 1        | 2'b10 | 2'b00 |  I-Type (19)
+| 0      | 1    | 2'b10  | 0        |        | 2'b11  | 1        |       | 2'b00 |  jal    (111)
+| 0      | 1    | 2'b10  | 0        |        | 2'b11  | 1        |       | 2'b01 |  csr    (115)
 
 
 - a la tabla de la guia a resSrc se le agrego un bit estra
@@ -24,17 +25,19 @@ module mainDeco(
     output wire aluSrc,
     output [1:0] inmSrc,
     output wire regWrite,
-    output [1:0] aluOp
+    output [1:0] aluOp,
+    output [1:0] mocsr
 );
 
-    reg branchAux;
-    reg [2:0] jumpAux;
-    reg [1:0] resSrcAux;
-    reg memWriteAux;
-    reg aluSrcAux;
-    reg [1:0] inmSrcAux;
-    reg regWriteAux;
-    reg [1:0] aluOpAux;
+    reg s_branch;
+    reg [2:0] s_jump;
+    reg [1:0] s_resSrc;
+    reg s_memWrite;
+    reg s_aluSrc;
+    reg [1:0] s_inmSrc;
+    reg s_regWrite;
+    reg [1:0] s_aluOp;
+    reg [1:0] s_mocsr;
 
     always @(*)
     begin
@@ -42,96 +45,118 @@ module mainDeco(
             // -- lw -------------------------------------------------------------------------------   
             3:
             begin
-                branchAux = 0;
-                jumpAux = 2'b01;
-                resSrcAux = 2'b01;
-                memWriteAux = 0;
-                aluSrcAux = 1;
-                inmSrcAux = 2'b00;
-                regWriteAux = 1;
-                aluOpAux = 2'b00;
+                s_branch = 0;
+                s_jump = 2'b01;
+                s_resSrc = 2'b01;
+                s_memWrite = 0;
+                s_aluSrc = 1;
+                s_inmSrc = 2'b00;
+                s_regWrite = 1;
+                s_aluOp = 2'b00;
+                s_mocsr = 2'b00;
             end
             // -- sw -------------------------------------------------------------------------------       
             35:
             begin
-                branchAux = 0;
-                jumpAux = 2'b01;
-                resSrcAux = 2'bx;
-                memWriteAux = 1;
-                aluSrcAux = 1;
-                inmSrcAux = 2'b01;
-                regWriteAux = 0;
-                aluOpAux = 2'b00;
+                s_branch = 0;
+                s_jump = 2'b01;
+                s_resSrc = 2'bx;
+                s_memWrite = 1;
+                s_aluSrc = 1;
+                s_inmSrc = 2'b01;
+                s_regWrite = 0;
+                s_aluOp = 2'b00;
+                s_mocsr = 2'b00;
             end
             // -- R-Type ---------------------------------------------------------------------------
             51:
             begin
-                branchAux = 0;
-                jumpAux = 2'b01;
-                resSrcAux = 2'b00;
-                memWriteAux = 0;
-                aluSrcAux = 0;
-                inmSrcAux = 2'bx;
-                regWriteAux = 1;
-                aluOpAux = 2'b10;
+                s_branch = 0;
+                s_jump = 2'b01;
+                s_resSrc = 2'b00;
+                s_memWrite = 0;
+                s_aluSrc = 0;
+                s_inmSrc = 2'bx;
+                s_regWrite = 1;
+                s_aluOp = 2'b10;
+                s_mocsr = 2'b00;
             end
             // -- B-Type ---------------------------------------------------------------------------
             99:
             begin
-                branchAux = 1;
-                jumpAux = 2'b01;
-                resSrcAux = 2'bx;
-                memWriteAux = 0;
-                aluSrcAux = 0;
-                inmSrcAux = 2'b10;
-                regWriteAux = 0;
-                aluOpAux = 2'b01;
+                s_branch = 1;
+                s_jump = 2'b01;
+                s_resSrc = 2'bx;
+                s_memWrite = 0;
+                s_aluSrc = 0;
+                s_inmSrc = 2'b10;
+                s_regWrite = 0;
+                s_aluOp = 2'b01;
+                s_mocsr = 2'b00;
             end
             // -- I-Type ---------------------------------------------------------------------------
             19:
             begin
-                branchAux = 0;
-                jumpAux = 2'b01;
-                resSrcAux = 2'b00;
-                memWriteAux = 0;
-                aluSrcAux = 1;
-                inmSrcAux = 2'b00;
-                regWriteAux = 1;
-                aluOpAux = 2'b10;
+                s_branch = 0;
+                s_jump = 2'b01;
+                s_resSrc = 2'b00;
+                s_memWrite = 0;
+                s_aluSrc = 1;
+                s_inmSrc = 2'b00;
+                s_regWrite = 1;
+                s_aluOp = 2'b10;
+                s_mocsr = 2'b00;
             end
             // -- jal ------------------------------------------------------------------------------     
             111:
             begin
-                branchAux = 0;
-                jumpAux = 2'b10;
-                resSrcAux = 2'b10;
-                memWriteAux = 0;
-                aluSrcAux = 1'bx;
-                inmSrcAux = 2'b11;
-                regWriteAux = 1;
-                aluOpAux = 2'bx;
+                s_branch = 0;
+                s_jump = 2'b10;
+                s_resSrc = 2'b10;
+                s_memWrite = 0;
+                s_aluSrc = 1'bx;
+                s_inmSrc = 2'b11;
+                s_regWrite = 1;
+                s_aluOp = 2'bx;
+                s_mocsr = 2'b00;
+            end
+            end
+            // -- CSR ------------------------------------------------------------------------------     
+            111:
+            begin
+                s_branch = 0;
+                s_jump = 2'b10;
+                s_resSrc = 2'b01;
+                s_memWrite = 0;
+                s_aluSrc = 1'bx;
+                s_inmSrc = 2'b11;
+                s_regWrite = 1;
+                s_aluOp = 2'bx;
+                s_mocsr = 2'b00; // por ahora, la salida sera csr_rd, despues vere lo de 2'b10->pc
             end
             default:
             begin
-                branchAux = 1'bx;
-                jumpAux = 2'b11;
-                resSrcAux = 2'bx;
-                memWriteAux = 1'bx;
-                aluSrcAux = 1'bx;
-                inmSrcAux = 2'bx;
-                regWriteAux = 1'bx;
-                aluOpAux = 2'bx;
+                s_branch = 1'bx;
+                s_jump = 2'b11;
+                s_resSrc = 2'bx;
+                s_memWrite = 1'bx;
+                s_aluSrc = 1'bx;
+                s_inmSrc = 2'bx;
+                s_regWrite = 1'bx;
+                s_aluOp = 2'bx;
+                s_mocsr = 2'b00;
             end
         endcase
     end
 
-    assign branch = branchAux;
-    assign jump = jumpAux;
-    assign resSrc = resSrcAux;
-    assign memWrite = memWriteAux;
-    assign aluSrc = aluSrcAux;
-    assign inmSrc = inmSrcAux;
-    assign regWrite = regWriteAux;
-    assign aluOp = aluOpAux;
+    assign branch = s_branch;
+    assign jump = s_jump;
+    assign resSrc = s_resSrc;
+    assign memWrite = s_memWrite;
+    assign aluSrc = s_aluSrc;
+    assign inmSrc = s_inmSrc;
+    assign regWrite = s_regWrite;
+    assign aluOp = s_aluOp;
+    assign mocsr = s_mocsr;
 
 endmodule
